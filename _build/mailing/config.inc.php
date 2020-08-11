35ef8d6ada12797d8398bc0d80822ae1e0d3bd81
Aleksey <a.goguev@alexgog.ru>
2020-08-11 17:12:55 +0300

    Work in progress

--- _build/mailing/config.inc.php
+++ _build/mailing/config.inc.php
@@ -1,8 +1,6 @@
 <?php
 
-require_once dirname(dirname(__FILE__)) . '/abstractmodule/config.inc.php';
-
 define('PKG_NAME', 'Mailing');
 define('PKG_NAME_LOWER', 'mailing');
-define('PKG_VERSION', '1.0');
-define('PKG_RELEASE', 'rc1');
+define('PKG_VERSION', '1.0.0');
+define('PKG_RELEASE', 'beta');
--- assets/components/mailing/connector.php
+++ assets/components/mailing/connector.php
@@ -1,19 +1,6 @@
 <?php
-require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
-require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
-require_once MODX_CONNECTORS_PATH . 'index.php';
 
-$corePath = $modx->getOption(
-    'mailing.core_path',
-    null,
-    $modx->getOption('core_path') . 'components/mailing/'
-);
-require_once $corePath . 'model/mailing/mailing.class.php';
-$modx->mailing = new Mailing($modx);
-$modx->lexicon->load('mailing:default');
+define('PKG_NAME', 'Mailing');
+define('PKG_NAME_LOWER', 'mailing');
 
-$path = $modx->getOption('processorsPath', $modx->mailing->config, $corePath . 'processors/');
-$modx->request->handleRequest(array(
-    'processors_path' => $path,
-    'location' => ''
-));
+require_once dirname(dirname(__FILE__)) . '/abstractmodule/connector.php';
